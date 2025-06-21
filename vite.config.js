9ab11c51a9a1c3a52e61b60d38d0f3f17c0c69eb
sylthalith <sylthalith@gmail.com>
2025-06-21 06:35:06 +0000

    Add vite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/index.css',
+                'resources/js/index.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0', // Доступ извне контейнера
+        port: 5173,
+        hmr: {
+            host: 'localhost', // Или 0.0.0.0 для доступа с хоста
+        },
+    },
 });
